71373885ca6c6198f267ad921ae110bb2d7378ac
HarshithSadhu <85260649+HarshithSadhu@users.noreply.github.com>
2023-04-11 14:10:00 -0500

    Test

--- priceTracker/ViewController.swift
+++ priceTracker/ViewController.swift
@@ -10,7 +10,18 @@ import WebKit
 import SwiftSoup
 
 class ViewController: UIViewController {
+    
+    /*
+     
+     let body: [String: Any] = ["asin": "asdfasdfas", "name": "Apple","price": 45, "priceLimit": 40, "deviceToken": deviceToken, "imageUrl": "https://m.media-amazon.com/images/I/816o-n47S8L._AC_SX679_.jpg"]
+     */
 
+    @IBOutlet weak var limField: UITextField!
+    
+    var productInfo = ["asin": "CHHFDSDF",
+                       "name": "",
+                       "price": 0.5,
+                       "priceLimit": 0.5, "success": false] as [String : Any]
     
     var token = ""
     @IBOutlet weak var addBtn: UIButton!
@@ -31,20 +42,14 @@ class ViewController: UIViewController {
         print("Current URL: ")
         print(currentURL)
         stringLink = currentURL!.absoluteString
-        apiCall()
+        
+        //getCall()
         scrapeAmazonProduct(linkThing: stringLink)
         
-        let controller = storyboard?.instantiateViewController(withIdentifier: "homePage") as! homePage
-        
-        controller.productImgLinks.append("https://media.wbur.org/wp/2=020/05/pencil-standardized-test-1000x667.jpg")
-        
-        controller.productData.append("Testt")
-        
-        print("current items")
-        print(controller.productData)
+       
         
         
-        performSegue(withIdentifier: "homeSegue", sender: self)
+        
         
         
     }
@@ -68,7 +73,10 @@ class ViewController: UIViewController {
 
         // create a URLSession and data task to fetch the HTML content asynchronously
         let session = URLSession.shared
-        let task = session.dataTask(with: url) { (data, response, error) in
+        
+        let group = DispatchGroup()
+            group.enter()
+        let task = session.dataTask(with: url) { (data, response, error) in
             if let error = error {
                 print("Error: \(error)")
                 return
@@ -89,7 +97,7 @@ class ViewController: UIViewController {
                 // get the title of the product
                 let title = try document.select("#productTitle").text()
                 print("Product Title: \(title)")
-
+        self.productInfo["name"] = title
                 // get the product rating
                 let rating = try document.select(".a-icon-star-small").first()?.text() ?? "N/A"
                 print("Product Rating: \(rating)")
@@ -98,7 +106,7 @@ class ViewController: UIViewController {
                 let price = try document.select(".a-price-whole").first()?.text() ?? "N/A"
                 let price2 = try document.select(".a-price-fraction").first()?.text() ?? "N/A"
                 print("Price: \(price + price2)")
-        
+        self.productInfo["price"] = Float(price + price2)
 
                 // get the review count
                 let reviewCount = try document.select("#acrCustomerReviewText").text()
@@ -107,20 +115,72 @@ class ViewController: UIViewController {
                 // get the product availability
                 let availability = try document.select("#availability").text()
                 print("Product Availability: \(availability)")
+        group.leave()
+        self.productInfo["success"] = true
             } catch {
                 print("Error: \(error)")
             }
         }
         
         // start the data task to fetch the HTML content
+        
         task.resume()
+        
+        
+        productInfo["priceLimit"] = Float(limField.text!)
+        
+        group.notify(queue: .main) {
+            self.apiCall()
+            }
+       
+        
+        
+        
+        
+        
+        
     }
     
     
     
+    func getCall(){
+        
+        // Create URL
+        let url = URL(string: "https://jhs-plutus.uc.r.appspot.com/get-products?page=0&limit=3&deviceToken=Sullivan")
+        guard let requestUrl = url else { fatalError() }
+        // Create URL Request
+        var request = URLRequest(url: requestUrl)
+        // Specify HTTP Method to use
+        request.httpMethod = "GET"
+        // Send HTTP Request
+        let task = URLSession.shared.dataTask(with: request) { (data, response, error) in
+            
+            // Check if Error took place
+            if let error = error {
+                print("Error took place \(error)")
+                return
+            }
+            
+            // Read HTTP Response Status code
+            if let response = response as? HTTPURLResponse {
+                print("Response HTTP Status code: \(response.statusCode)")
+            }
+            
+            // Convert HTTP Response Data to a simple String
+            if let data = data, let dataString = String(data: data, encoding: .utf8) {
+                print("Response data string:\n \(dataString)")
+            }
+            
+        }
+        task.resume()
+        
+        
+        
+    }
     
     
     func apiCall(){
+        
         
         guard let url = URL(string: "https://jhs-plutus.uc.r.appspot.com/add-product") else{
             return
@@ -135,12 +195,17 @@ class ViewController: UIViewController {
         //let body: [String: String] = ["link": stringLink]
         
         let deviceToken = UIDevice.current.identifierForVendor?.uuidString
+        
+        
 
         
-        let body: [String: Any] = ["asin": "asdfasdfas", "name": "Apple","price": 45, "priceLimit": 40, "deviceToken": deviceToken]
+        let body: [String: Any] = ["asin": productInfo["asin"], "name": productInfo["name"],"price": productInfo["price"], "priceLimit": productInfo["priceLimit"], "deviceToken": deviceToken, "imageUrl": "https://m.media-amazon.com/images/I/816o-n47S8L._AC_SX679_.jpg"]
+        
+        let body2: [String: Any] = ["page": 1, "limit": 3, "deviceToken": deviceToken, "sortBy": "name", "sortOrder": "asc"]
         
         
         request.httpBody = try? JSONSerialization.data(withJSONObject: body, options: .fragmentsAllowed)
+        
         // Make the request
         let task = URLSession.shared.dataTask(with: request){ data, _, error in
             guard let data = data, error == nil else{
@@ -160,6 +225,8 @@ class ViewController: UIViewController {
             }
             
         }
+        
+        performSegue(withIdentifier: "homeSegue", sender: self)
         task.resume()
         
         
--- productCell.swift
+++ productCell.swift
@@ -12,13 +12,16 @@ class productCell: UICollectionViewCell {
     @IBOutlet weak var productDetails: UILabel!
     @IBOutlet weak var productImg: UIImageView!
     
+    @IBOutlet weak var pricelim: UILabel!
+    @IBOutlet weak var priceLbl: UILabel!
     
     
     
-    public func configure(with image: UIImage, productName: String){
+    public func configure(with image: UIImage, productName: String, productPrice:Float, productLim:Float){
         productImg.image = image
         productDetails.text = productName
-        
+        priceLbl.text = "Price: " + String(productPrice)
+        pricelim.text = "Lim: " + String(productLim)
         productImg.layer.cornerRadius = 5.0
         productImg.layer.masksToBounds = true
         
